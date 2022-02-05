a021779c6e62b7ab2a158b1fd92b64cafe30637a
Anil Nanda <anilthekkoote@gmail.com>
2022-02-05 16:34:26 +0530

    Getting single id from GET request

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from random import randrange
 from typing import Optional
 from fastapi import FastAPI
 #from fastapi.params import Body
 from pydantic import BaseModel
 
 
-my_post = [{"title":"First Post","content":"First Post content"}]
+my_post = [{"id":1,"title":"First Post","content":"First Post content"}]
 
 app = FastAPI()
 
@@ -14,6 +15,10 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+def find_post(id):
+    for post in my_post:
+        if post["id"] == id:
+            return post
 
 @app.get("/")
 def getPost():
@@ -26,5 +31,16 @@ def getPost():
 
 @app.post("/posts")
 def createPost(payload: Post):
-    print(payload,payload.dict())
-    return {"message": f"title: {payload.title}, content {payload.content}"}
+    #print(payload,payload.dict())
+    post = payload.dict()
+    post["id"] = randrange(0,10000)
+    my_post.append(post)
+    return {"data": post}
+
+@app.get("/posts/{id}")
+def getpost(id: int):
+    post = find_post(id)
+    return {"data": post}
+
+
+    
