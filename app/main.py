bcfaad012b28bab99ea1ad0d7017b469e0eff9f1
Anil Nanda <anilthekkoote@gmail.com>
2022-02-05 19:51:18 +0530

    Moved main.py to app dir
    Moved main.py to app dir

--- app/main.py
+++ app/main.py
@@ -10,10 +10,13 @@ my_post = [{"id":1,"title":"First Post","content":"First Post content"}]
 
 app = FastAPI()
 
+# Definig a schema for the request body
 class Post(BaseModel):
     title: str
     content: str
+    # Default value is set as True
     published: bool = True
+    # Set the rating as an optional parameter with default value None
     rating: Optional[int] = None
 
 def find_post(id):
@@ -35,7 +38,10 @@ def getPost():
 def getPost():
     return {"data" : my_post}
 
+# Default response status is set as parameter to the decorator
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
+
+# The body of the request is stored in variable payload and validated again the schema Post defined above
 def createPost(payload: Post):
     #print(payload,payload.dict())
     post = payload.dict()
@@ -44,6 +50,8 @@ def createPost(payload: Post):
     return {"data": post}
 
 @app.get("/posts/{id}")
+
+# id: int ensures the id value passed in the request is int type
 def getpost(id: int):
     post = find_post(id)
     if not post:
@@ -58,6 +66,7 @@ def deletepost(id: int):
     if index is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
     my_post.pop(index)
+    # delete request can't return any response back to user, it can return only a status
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
