64ccbb1668980d52a40cc13777a7a86c25d56279
Anil Nanda <anilthekkoote@gmail.com>
2022-02-05 12:27:18 +0530

    Added GET and POST method handling

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
+my_post = [{"title":"First Post","content":"First Post content"}]
 
 app = FastAPI()
 
 @app.get("/")
 def getPost():
-    return "Hello World!"
+    return {"message":"Hello World!"}
+
+
+@app.get("/posts")
+def getPost():
+    return {"data" : my_post}
+
+@app.post("/posts")
+def createPost(payload: dict = Body(...)):
+    return {"message":payload}
