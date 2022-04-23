28c5e421ccdfab2e319e3133109195e860397d8a
Anil Nanda <anilthekkoote@gmail.com>
2022-04-23 15:45:03 +0400

    Using fixtures to remove repeated code in test cases

--- app/apptest.py
+++ app/apptest.py
@@ -4,3 +4,16 @@ def add(a,b):
 
 def sub(a,b):
     return a-b
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+    
+    def collect_interest(self):
+        self.balance *= 1.1
--- unittests/test_app.py
+++ unittests/test_app.py
@@ -1,4 +1,4 @@
-from app.apptest import add, sub
+from app.apptest import add, sub, BankAccount
 import pytest
 
 @pytest.mark.parametrize("num1,num2,expected",[
@@ -12,3 +12,33 @@ def test_add(num1, num2, expected):
 
 def test_sub():
     assert sub(8,3) == 5
+
+
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+#The fixture function is passed as parameter to testing function and same name is referred as return object
+def test_starting_balance(bank_account): 
+    assert bank_account.balance == 50
+
+def test_bank_default_amount(zero_bank_account):
+    assert  zero_bank_account.balance == 0
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+def test_deposit():
+    bankaccount = BankAccount(50)
+    bankaccount.deposit(20)
+    assert bankaccount.balance == 70
+
+def test_collect_interest():
+    bankaccount = BankAccount(50)
+    bankaccount.collect_interest()
+    assert round(bankaccount.balance,6) == 55
